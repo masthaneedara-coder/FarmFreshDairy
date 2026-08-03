485dcfde2dedc666aea005339fb0a50b43075ba7
Masthan <masthan.eedara@gmail.com>
2026-08-03 13:31:55 +0530

    update

--- src/config/api.js
+++ src/config/api.js
@@ -1,4 +1,5 @@
-const API_URL = "http://localhost:5000/api";
+//const API_URL = "http://localhost:5000/api";
+const API_URL = "https://farmfreshdairy.onrender.com/api";
 /* ---------------------------------------
    COMMON HELPERS
 --------------------------------------- */
