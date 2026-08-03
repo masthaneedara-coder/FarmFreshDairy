bad08c0bc264fae709aaf235ac2ac07aedd0aca2
Masthan <masthan.eedara@gmail.com>
2026-08-03 15:24:25 +0530

    Update production COR223

--- src/Components/DeliveryRoute.jsx
+++ src/Components/DeliveryRoute.jsx
@@ -10,7 +10,7 @@ export default function DeliveryRoute({ children }) {
       location.pathname
     );
 
-    return <Navigate to="/auth" replace />;
+    return <Navigate to="/delivery-login" replace />;
   }
 
   return children;
