9873a738d5a3718d5479ef21d1584f16083757d3
Masthan <masthan.eedara@gmail.com>
2026-09-09 11:39:10 +0530

    Allow Capacitor Android origin

--- backend/src/app.js
+++ backend/src/app.js
@@ -47,7 +47,7 @@ app.use(helmet());
 // Enable CORS
 const allowedOrigins = [
   "http://localhost:5173",
-  "http://localhost",
+   "https://localhost",
   "https://farm-fresh-dairy.vercel.app",
 ];
 
